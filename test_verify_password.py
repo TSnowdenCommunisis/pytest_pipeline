19f246582a5e9b4e9ad7691d8a4a1e4f663d3d19
TolgaAltinors <50206787+TolgaAltinors@users.noreply.github.com>
2021-02-19 11:20:04 +0000

    fix the test script

--- test_verify_password.py
+++ test_verify_password.py
@@ -19,15 +19,15 @@ def test_has_lower():
     assert verify_password.has_lower("dHjflfH23jlhhh") == True
     assert verify_password.has_lower("HHHHHHHHHH") == False
 
-#def test_has_upper():
-#
-#    assert verify_password.has_upper("HHHHHHHHHH") == True
-#    assert verify_password.has_upper("hjsnuhn235") == False 
+def test_has_upper():
+
+    assert verify_password.has_upper("HHHHHHHHHH") == True
+    assert verify_password.has_upper("hjsnuhn235") == False 
 
 def test_make_upper():
 
     result = verify_password.make_upper("Hello World")
-    assert result == "HELLO WORL"
+    assert result == "HELLO WORLD"
     assert type(result) is str
     assert "Wow" not in result
 
